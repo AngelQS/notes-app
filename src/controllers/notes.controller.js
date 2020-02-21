48851d94a4f7a7e474093973e0ed8ed3a043c51b
AngelQS <aedwin.acuario31@gmail.com>
2020-02-21 14:28:38 -0500

    Añadida validacion de permisos de usuario para crear y editar notas.

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -9,19 +9,30 @@ notesCtrl.renderNoteForm = (req, res) => {
 
 notesCtrl.createNewNote = async (req, res) => {
   const { title, description } = req.body;
-  const newNote = new Note({ title, description });
+  const newNote = new Note({
+    title,
+    description,
+  });
+  // el id puede ser obtenido ya sea desde req.user._id o req.user.id
+  newNote.user = req.user._id;
   await newNote.save();
   req.flash('success_msg', 'Note has been created.');
   res.redirect('/notes');
 };
 
 notesCtrl.renderNotes = async (req, res) => {
-  const notes = await Note.find();
+  const notes = await Note.find({ user: req.user._id }).sort({
+    createdAt: 'desc',
+  });
   res.render('notes/all-notes', { notes });
 };
 
 notesCtrl.renderEditForm = async (req, res) => {
   const note = await Note.findById(req.params.id);
+  if (note.user != req.user._id) {
+    req.flash('error_msg', 'Not authorized. You can only edit your own notes.');
+    return res.redirect('/notes');
+  }
   res.render('notes/edit-note', { note });
 };
 
